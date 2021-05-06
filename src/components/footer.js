63d6d48a71763fc09855a066f908a527ead097f4
Lalita <46801379+kesaviweb@users.noreply.github.com>
2021-05-06 09:24:51 +0530

    Changed credits in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        A website for EmpowerWIT, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="https://kesaviweb.com">kesaviweb.com</Link>
       </p>
     </div>
   </footer>
